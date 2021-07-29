47c5baaa62d8d0c07c86a0d0a455ede7c511183c
Nikolay Andreev <bakasaru@list.ru>
2021-07-29 22:02:18 +0300

    Add try-except for NodesGuru API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from prometheus_client import start_http_server, Gauge, Info
 import time
 import requests
+import json
 
 packets_mixed_total = Gauge('nym_packets_mixed_total', 'Packets mixed since startup')
 packets_mixed_last = Gauge('nym_packets_mixed_last', 'Packets mixed in last 30 seconds')
@@ -10,14 +11,20 @@ meta = Info('nym_meta', 'Information about node')
 
 def get_metrics(mixer_ip):
     stats = requests.get(f'http://{mixer_ip}:8000/stats').json()
-    info = requests.get(f'https://nodes.guru/api/nym/mixnode?q={mixer_ip}&limit=6').json()
 
     total_mixed = stats['packets_sent_since_startup']
     current_mixed = stats['packets_sent_since_last_update']
 
     packets_mixed_total.set(total_mixed)
     packets_mixed_last.set(current_mixed)
-    meta.info({k: str(v) for k, v in info['metrics'].items()})
+
+    try:
+        info = requests.get(f'https://nodes.guru/api/nym/mixnode?q={mixer_ip}&limit=6').json()
+        meta.info({k: str(v) for k, v in info['metrics'].items()})
+    except (requests.ConnectionError, requests.Timeout):
+        print('Error while connecting to NodesGuru Nym API')
+    except json.decoder.JSONDecodeError:
+        print('Error while parsing response from NodesGuru Nym API')
 
 
 if __name__ == '__main__':
